2ce389a24cb46e8f9ea3a6eaf1dd8c86f8ff5a60
hyojekim <42.4.hyojekim@gmail.com>
2022-03-21 20:29:42 +0900

    feat: :sparkles: Update camera setting
    카메라가 보는 방향과 시야각 정의하기
    
    #9

--- tutorial/src/scene/canvas.c
+++ tutorial/src/scene/canvas.c
@@ -6,6 +6,6 @@ t_canvas	canvas(int width, int height)
 
 	canvas.width = width;
 	canvas.height = height;
-	canvas.aspect_ratio = (double)width / (double)height;
+	canvas.aspect_ratio = (double)height / (double)width;
 	return (canvas);
 }
