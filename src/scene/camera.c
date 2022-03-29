91c62449be766dda9d93a877bb62962d212b64f2
hyojekim <42.4.hyojekim@gmail.com>
2022-03-29 14:15:40 +0900

    fix: :bug: Fix camera orientation error
    카메라 방향벡터에서 발생하는 오류 수정
    
    #30

--- src/scene/camera.c
+++ src/scene/camera.c
@@ -1,12 +1,7 @@
+#include <math.h>
 #include "parse.h"
 #include "scene.h"
 #include "vector3.h"
-#include <math.h>
-
-static double	degree_to_radian(double degree)
-{
-	return (degree * M_PI / 180);
-}
 
 t_camera	camera_prev_set(t_scene *scene, t_obj_list *lst)
 {
@@ -24,19 +19,34 @@ t_camera	camera_prev_set(t_scene *scene, t_obj_list *lst)
 	return (camera);
 }
 
+static double	degree_to_radian(double degree)
+{
+	return (degree * M_PI / 180);
+}
+
+static t_vec3	upside_set(t_vec3 w)
+{
+	t_vec3	default_upside;
+	t_vec3	u;
+
+	default_upside = vec3(0, 1, 0);
+	u = vec3_cross(default_upside, w);
+	if (vec3_length(u) == 0)
+		u = vec3_cross(vec3(0, 0, 1), w);
+	return (vec3_unit(u));
+}
+
 t_camera
 	camera_set(t_canvas *canvas, t_point3 origin, t_vec3 direct, double h_fov)
 {
 	t_camera	cam;
 	double		theta;
 	double		field_width;
-	t_vec3		default_upside;
 
 	theta = degree_to_radian(h_fov);
 	field_width = tan(theta / 2) * 2.0;
-	default_upside = vec3(0, 1, 0);
 	cam.w_direction = direct;
-	cam.u_upside = vec3_unit(vec3_cross(default_upside, cam.w_direction));
+	cam.u_upside = upside_set(cam.w_direction);
 	cam.v_cross = vec3_unit(vec3_cross(cam.w_direction, cam.u_upside));
 	cam.origin = origin;
 	cam.viewport_width = field_width;
