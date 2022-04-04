e9506d5ee96061202dd115e3627f25e7495d6d6f
hyojekim <42.4.hyojekim@gmail.com>
2022-04-04 22:49:06 +0900

    feat: :sparkles: Add checkerboard for objects
    .rt에서 받아온 ck 정보로 checkerboard 입히기
    
    #54

--- src_bonus/trace/hit_plane_bonus.c
+++ src_bonus/trace/hit_plane_bonus.c
@@ -1,5 +1,20 @@
+#include <math.h>
 #include "trace_bonus.h"
 #include "vector3_bonus.h"
+
+static void	plane_uv(t_point3 p, t_vec3 normal, double *u, double *v)
+{
+	t_vec3	u_dir;
+	t_vec3	v_dir;
+
+	v_dir = vec3_cross(normal, vec3(1, 0, 0));
+	if (vec3_length_square(v_dir) == 0)
+		v_dir = vec3_cross(normal, vec3(0, 0, 1));
+	v_dir = vec3_unit(v_dir);
+	u_dir = vec3_unit(vec3_cross(v_dir, normal));
+	*u = fmod(vec3_dot(p, u_dir), 1);
+	*v = fmod(vec3_dot(p, v_dir), 1);
+}
 
 t_bool	hit_plane(t_obj_list objects[], t_ray *ray, t_hit_record *rec)
 {
@@ -19,7 +34,11 @@ t_bool	hit_plane(t_obj_list objects[], t_ray *ray, t_hit_record *rec)
 	rec->t = root;
 	rec->p = ray_at(ray, root);
 	rec->normal = pl->normal;
+	plane_uv(rec->p, rec->normal, &rec->u, &rec->v);
+	if (is_checkerboard(objects->color))
+		rec->color = checker_color(rec->u, rec->v, objects->color);
+	else
+		rec->color = objects->color.color;
 	set_face_normal(ray, rec);
-	rec->color = objects->color.color;
 	return (TRUE);
 }
--- src_bonus/trace/hit_sphere_bonus.c
+++ src_bonus/trace/hit_sphere_bonus.c
@@ -2,7 +2,18 @@
 #include "trace_bonus.h"
 #include "vector3_bonus.h"
 
-static t_bool	check_sphere(
+static void	sphere_uv(t_vec3 normal, double *u, double *v)
+{
+	double	theta;
+	double	phi;
+
+	theta = acos(-1 * normal.y);
+	phi = atan2(-1 * normal.z, normal.x) + M_PI;
+	*u = phi * M_1_PI * 0.5;
+	*v = theta * M_1_PI;
+}
+
+static t_bool	sphere_check(
 		t_obj_list objects[], t_ray *ray, t_hit_record *rec, double root)
 {
 	t_sphere	*sp;
@@ -13,7 +24,11 @@ static t_bool	check_sphere(
 	rec->t = root;
 	rec->p = ray_at(ray, root);
 	rec->normal = vec3_unit(vec3_minus(rec->p, sp->center));
-	rec->color = objects->color.color;
+	sphere_uv(rec->normal, &rec->u, &rec->v);
+	if (is_checkerboard(objects->color))
+		rec->color = checker_color(rec->u, rec->v, objects->color);
+	else
+		rec->color = objects->color.color;
 	set_face_normal(ray, rec);
 	return (TRUE);
 }
@@ -37,9 +52,9 @@ t_bool	hit_sphere(t_obj_list objects[], t_ray *ray, t_hit_record *rec)
 	t_eq	eq;
 
 	eq = sphere_equation(objects, ray);
-	if (check_sphere(objects, ray, rec, root(eq, ALPHA)))
+	if (sphere_check(objects, ray, rec, root(eq, ALPHA)))
 		return (TRUE);
-	if (check_sphere(objects, ray, rec, root(eq, BETA)))
+	if (sphere_check(objects, ray, rec, root(eq, BETA)))
 		return (TRUE);
 	return (FALSE);
 }
