3793a30b706e1d94c8b72f5651eef2a3ddc0be24
Chang hyun Jang <cjang@student.42seoul.kr>
2022-04-12 14:51:15 +0900

    fix: :bug: Fix scene.h unnecessary function
    scene.h에서 불필요한 함수 제거
    
    #74

--- include/scene.h
+++ include/scene.h
@@ -8,12 +8,6 @@
 t_canvas	canvas_set(int width, int height);
 t_scene		*new_scene(int width, int height);
 
-// object.c
-
-t_obj_list	*new_object(t_obj_type type, void *object, t_color3 color);
-t_light		*new_point_light(t_point3 light_origin, double bright_ratio);
-t_sphere	*new_sphere(t_point3 center, double radius);
-
 // ambient
 
 t_ambient	ambient_set(t_obj_list *lst);
