4bed78330506609fb1530318f68349550b61ee33
Chang hyun Jang <cjang@student.42seoul.kr>
2022-04-17 19:33:22 +0900

    fix: :bug: Fix RGB parse to int.
    RGB값은 integer로만 들어오는 걸로
    
     #102

--- lib/libft/include/ft_ascii.h
+++ lib/libft/include/ft_ascii.h
@@ -4,6 +4,7 @@
 # include "libft_type.h"
 
 double	ft_atof(char *str, int *valid);
+int		ft_atoi(const char *str, t_res *valid);
 t_bool	ft_isdigit(int c);
 t_bool	ft_isspace(int c);
 
--- src_bonus/scene/ambient_bonus.c
+++ src_bonus/scene/ambient_bonus.c
@@ -1,6 +1,5 @@
 #include "parse_bonus.h"
 
-// rgb가 실수로 들어올때에 대해 고민해봐야함
 t_ambient	ambient_set(t_parse_list *lst)
 {
 	t_parse		*lst_parse;
@@ -8,6 +7,6 @@ t_ambient	ambient_set(t_parse_list *lst)
 
 	lst_parse = lst->object;
 	ambient.light_ratio = double_get(lst_parse->bri_ratio, 0.0, 1.0);
-	ambient.light_color = vec_get(lst_parse->rgb, 0, 255);
+	ambient.light_color = vec_int_get(lst_parse->rgb, 0, 255);
 	return (ambient);
 }
