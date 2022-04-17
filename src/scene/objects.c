1cf1e40932f09ec9d02713fe965ce3a7d69f838f
Chang hyun Jang <cjang@student.42seoul.kr>
2022-04-17 20:51:09 +0900

    fix: :bug: Fix RGB parse to int from mandatory.
    #102
    
    #102

--- src/scene/objects.c
+++ src/scene/objects.c
@@ -14,11 +14,11 @@ void	sphere_set(t_scene *scene, t_parse_list *lst)
 	lst_new = ft_calloc(sizeof(t_obj_list), 0);
 	sphere = ft_calloc(sizeof(t_sphere), 0);
 	obj_list_add_back(&scene->objects, lst_new);
-	sphere->center = vec_get(lst_parse->point, 0, 0);
+	sphere->center = vec_double_get(lst_parse->point, 0, 0);
 	sphere->radius = double_get(lst_parse->diameter, 0, INFINITY) / 2;
 	sphere->radius2 = sphere->radius * sphere->radius;
 	lst_new->type = SPHERE;
-	lst_new->color = vec_get(lst_parse->rgb, 0, 255);
+	lst_new->color = vec_int_get(lst_parse->rgb, 0, 255);
 	lst_new->object = sphere;
 }
 
@@ -32,10 +32,10 @@ void	plane_set(t_scene *scene, t_parse_list *lst)
 	lst_new = ft_calloc(sizeof(t_obj_list), 0);
 	plane = ft_calloc(sizeof(t_plane), 0);
 	obj_list_add_back(&scene->objects, lst_new);
-	plane->point = vec_get(lst_parse->point, 0, 0);
-	plane->normal = vec_get(lst_parse->nor_vec, -1, 1);
+	plane->point = vec_double_get(lst_parse->point, 0, 0);
+	plane->normal = vec_double_get(lst_parse->nor_vec, -1, 1);
 	lst_new->type = PLANE;
-	lst_new->color = vec_get(lst_parse->rgb, 0, 255);
+	lst_new->color = vec_int_get(lst_parse->rgb, 0, 255);
 	lst_new->object = plane;
 }
 
@@ -49,12 +49,12 @@ void	cylinder_set(t_scene *scene, t_parse_list *lst)
 	lst_new = ft_calloc(sizeof(t_obj_list), 0);
 	cylinder = ft_calloc(sizeof(t_cylinder), 0);
 	obj_list_add_back(&scene->objects, lst_new);
-	cylinder->center = vec_get(lst_parse->point, 0, 0);
-	cylinder->normal = vec_get(lst_parse->nor_vec, -1, 1);
+	cylinder->center = vec_double_get(lst_parse->point, 0, 0);
+	cylinder->normal = vec_double_get(lst_parse->nor_vec, -1, 1);
 	cylinder->radius = double_get(lst_parse->diameter, 0, INFINITY) / 2;
 	cylinder->radius2 = cylinder->radius * cylinder->radius;
 	cylinder->height = double_get(lst_parse->height, 0, INFINITY);
 	lst_new->type = CYLINDER;
-	lst_new->color = vec_get(lst_parse->rgb, 0, 255);
+	lst_new->color = vec_int_get(lst_parse->rgb, 0, 255);
 	lst_new->object = cylinder;
 }
